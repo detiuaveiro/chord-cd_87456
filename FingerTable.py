c76e93c529ae72daf2078902e3590620c2b7d72e
Jose Carlos <jose.carlos94@ua.pt>
2020-03-26 19:14:14 +0000

    Fingertable teste.

--- FingerTable.py
+++ FingerTable.py
@@ -1,24 +1,41 @@
-from utils import dht_hash, contains_predecessor, contains_successor
+from utils import contains_successor
 
 
 class FingerTable:
-    def __init__(self, succ_addr):
-        self.ft = [succ_addr]
-        for i in range(1, 10):
-            self.ft.append(None)
-        self.update_index = 0
+    def __init__(self, scss_id=None, scss_addr=None):
+        self.index_entry = 0
+        self.finger_Table = []
+        self.entries = 10
+        for n in range(self.entries):
+            self.finger_Table.append({'id': scss_id, 'addr': scss_addr})
 
-    def update_index(self, index, addr):
-        self.ft[i] = addr
-        i = (i + 1) % len(self.ft)
+    def closest_preceding_node(self, node_id, test_id):
+        for e in reversed(self.finger_Table):
+            ft_id = e.get('id')
+            if contains_successor(node_id, ft_id, test_id):
+                return e
+        return None
 
-    def get_update_index(self):
-        return self.update_index
+    def entry_ft(self):
+        previous = self.index_entry + 2
+        self.index_entry = (self.index_entry + 1) % 8
+        return previous
 
-    def closest_preceding_node(self, node_id):
-        # ainda nao tem em conta os ids serem circulares
-        print(self.ft)
-        for entry in self.ft[::-1]:
-            if entry and dht_hash(entry.__str__()) < node_id:
-                print(entry)
-                return entry
+    def update_ft(self, index, node_id, node_addr):
+        self.finger_Table[index] = {'id': node_id, 'addr': node_addr}
+
+    def get_succ_addr(self):
+        return self.finger_Table[0].get('addr')
+
+    def get_succ_id(self):
+        return self.finger_Table[0].get('id')
+
+    def __str__(self):
+        s = '\n'
+        for e in self.finger_Table:
+            s += 'id: ' + str(e.get('id')) + ' with addr: ' + str(e.get('addr')) + '\n'
+
+        return s
+
+    def __repr(self):
+        return self.__str__()
